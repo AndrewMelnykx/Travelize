68989f746eb8662649b10afa1f2743f9b65ef3d9
Andrew Melnykx <andreymelnyk777@gmail.com>
2024-08-24 13:42:56 +0200

    Fixed link/CORS problem

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://travelize-frontend.onrender.com",
     credentials: true,
   })
 );
