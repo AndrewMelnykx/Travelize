e0a8f5bc1662a0c9ac7e6b627c5395cfbcdda221
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-12-31 08:51:06 +0100

    Change letters for deploy

--- client/src/routes/TravelPlan/filter/Filter.tsx
+++ client/src/routes/TravelPlan/filter/Filter.tsx
@@ -9,7 +9,7 @@ import {
 
 import { Box, useMediaQuery } from "@mui/material";
 import CustomSelect from "@custom-ui/select";
-import CustomInput from "@custom-ui/input";
+import CustomInput from "@custom-ui/Input";
 import TravelAutocomplete from "./Autocomplete";
 import TravelFilterCustomButton from "./Button";
 
